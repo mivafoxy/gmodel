01ca47f977b7488b6ae9cd8ed7d61252a6e53c4c
mivafoxy <creatox1996@gmail.com>
2019-05-13 20:28:04 +0300

    Очередной бесполезный комментарий и удаление лишнего файла

--- lab3/createTasks.m
+++ lab3/createTasks.m
@@ -4,7 +4,7 @@ tn = 1;
 tk = 10;
 q = queue();
 for i=1:1:n
-    t = tn + (tk-tn)*(-log(rand)/lambda);
+    t = tn + (tk-tn)*(-log(rand)/lambda); % Тоже какая то неведомая хрень.
     ts = task(i,t);
     q.add(ts);
 end
